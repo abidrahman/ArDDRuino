d8ca352262f14dc49aea4a761e8d195c29e656a5
Megasonic988 <weixiang@ualberta.ca>
2015-11-12 16:09:10 -0700

    as

--- MenuState.h
+++ MenuState.h
@@ -1,12 +1,6 @@
 #ifndef ____MenuState__
 #define ____MenuState__
 
-struct Joystick {
-    int delta_vert;
-    int delta_horiz;
-    boolean pushed;
-};
-
 void updateMenuState(unsigned long dt);
 void renderMenuState(Adafruit_ST7735 &tft, Joystick joystick);
 
