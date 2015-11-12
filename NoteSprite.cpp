694afff0c3079f800a13847f4c06c7706245edfb
Abid Rahman <arahman1@ualberta.ca>
2015-11-12 13:15:49 -0700

    chANGEEEE

---
+++ NoteSprite.cpp
@@ -0,0 +1,31 @@
+#include <NoteSprite.h>
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <SPI.h>
+#include <SD.h>
+
+#include "lcd_image.h"
+
+// standard U of A library settings, assuming Atmel Mega SPI pins
+#define SD_CS    5  // Chip select line for SD card
+#define TFT_CS   6  // Chip select line for TFT display
+#define TFT_DC   7  // Data/command line for TFT
+#define TFT_RST  8  // Reset line for TFT (or connect to +5V)
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+void render () {
+	
+	
+	
+	
+	
+}
+
+void update (int dt) {
+
+	lcd_image_t map_image = { "yeg-sm.lcd", 128, 128 };
+
+
+}
---
+++ NoteSprite.h
@@ -0,0 +1,20 @@
+#ifndef NOTESPRITE_CPP
+#define NOTESPRITE_CPP
+
+class NoteSprite
+{
+	public:
+	struct NoteSprite (int x, int y) {
+		
+		int x;
+		int y;
+		int vy;
+		
+	};
+	
+	void update (int dt);
+	
+};
+
+
+#endif
