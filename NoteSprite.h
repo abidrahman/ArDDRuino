cc53c4df41a70e8a296c53cd0eba964b727f4f85
Megasonic988 <weixiang@ualberta.ca>
2015-11-12 19:26:55 -0700

    d

--- NoteSprite.h
+++ NoteSprite.h
@@ -1,6 +1,13 @@
 #ifndef NOTESPRITE_CPP
 #define NOTESPRITE_CPP
 
+#include <Arduino.h>
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <SPI.h>
+#include <SD.h>
+#include "lcd_image.h"
+
 class NoteSprite
 {
 public:
